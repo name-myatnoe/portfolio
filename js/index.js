f190fbe09d31e1d823295aa857741855d0041650
name-myatnoe <eaintmyatnoe0517@gmail.com>
2025-12-13 23:26:38 +0630

    refactored the code for readability

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 const mobileNavToggleBtn = document.querySelector(".mobile-nav-toggle");
 const mobileNavLinks = document.querySelector(".mobile-nav-links");
-const mobileNavToggleLine2 = document.querySelector(".mobile-nav-toggle-line2");
 const mobileNavToggleLine1 = document.querySelector(".mobile-nav-toggle-line1");
+const mobileNavToggleLine2 = document.querySelector(".mobile-nav-toggle-line2");
 const mobileNavToggleLine3 = document.querySelector(".mobile-nav-toggle-line3");
 
 mobileNavToggleBtn.addEventListener("click", () => {
-    mobileNavToggleLine2.classList.toggle("active-mobile-nav-toggle-line2");
-    mobileNavToggleLine1.classList.toggle("active-mobile-nav-toggle-line1");
-    mobileNavToggleLine3.classList.toggle("active-mobile-nav-toggle-line3");
     mobileNavLinks.classList.toggle("active-mobile-nav-links");
+    
+    mobileNavToggleLine1.classList.toggle("active-mobile-nav-toggle-line1");
+    mobileNavToggleLine2.classList.toggle("active-mobile-nav-toggle-line2");
+    mobileNavToggleLine3.classList.toggle("active-mobile-nav-toggle-line3");
 })
 
 mobileNavLinks.addEventListener("click", (e)=> {
