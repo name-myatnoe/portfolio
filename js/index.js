4e212052bc7d59ef0abde8e7cd6dca4c704d0c48
name-myatnoe <eaintmyatnoe0517@gmail.com>
2025-12-26 22:32:24 +0630

    update hero section and experiences section

--- js/index.js
+++ js/index.js
@@ -1,24 +1,25 @@
 const mobileNavToggleBtn = document.querySelector(".mobile-nav-toggle");
 const mobileNavLinks = document.querySelector(".mobile-nav-links");
-const mobileNavToggleLine1 = document.querySelector(".mobile-nav-toggle-line1");
-const mobileNavToggleLine2 = document.querySelector(".mobile-nav-toggle-line2");
-const mobileNavToggleLine3 = document.querySelector(".mobile-nav-toggle-line3");
+const mobileNavToggleLine2 =  document.querySelector(".mobile-nav-toggle-line-2");
+const mobileNavToggleLine3 =  document.querySelector(".mobile-nav-toggle-line-3");
+const mobileNavToggleLine1 =  document.querySelector(".mobile-nav-toggle-line-1");
+
 
 function toggleMobileNav(){
     mobileNavLinks.classList.toggle("active-mobile-nav-links");
-    
-    mobileNavToggleLine1.classList.toggle("active-mobile-nav-toggle-line1");
-    mobileNavToggleLine2.classList.toggle("active-mobile-nav-toggle-line2");
-    mobileNavToggleLine3.classList.toggle("active-mobile-nav-toggle-line3");
-}
 
-mobileNavToggleBtn.addEventListener("click", () => {
+    // annimation 
+    mobileNavToggleLine2.classList.toggle("active-mobile-nav-toggle-line-2");
+    mobileNavToggleLine1.classList.toggle("active-mobile-nav-toggle-line-1");
+    mobileNavToggleLine3.classList.toggle("active-mobile-nav-toggle-line-3");
+}
+mobileNavToggleBtn.addEventListener("click" , ()=> {
     toggleMobileNav();
 })
 
-mobileNavLinks.addEventListener("click", (e)=> {
-    if(e.target.tagName === "a"){
-        mobileNavLinks.classList.toggle("active-mobile-nav-links");
+
+mobileNavLinks.addEventListener("click" , (e) => {
+    if(e.target.tagName === "A"){
+       toggleMobileNav();
     }
 })
-console.log(mobileNavToggleBtn);
